6f8a9298f2e87ad20c6a9e52382bcb298172a253
jonnyF <fuhuixiang123@vip.qq.com>
2015-04-08 09:45:27 +0800

    将本地的静态文件转移到七牛云存储上

--- FlaskTest.py
+++ FlaskTest.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    qiniuurl = url_for_qiniu()
+    return render_template('index.html', url=qiniuurl)
 
 @app.route('/<url>')
 def index(url=None):
@@ -13,12 +14,14 @@ def index(url=None):
         template = 'about.html'
     else:
         template = 'index.html'
-    return render_template(template)
+    qiniuurl = url_for_qiniu()
+    return render_template(template, url=qiniuurl)
 
 
 @app.route('/about/')
 def abouts():
-    return render_template('about.html')
+    qiniuurl = url_for_qiniu()
+    return render_template('about.html', url=qiniuurl)
 
 
 @app.route('/about/<url>')
@@ -31,15 +34,18 @@ def about(url=None):
         template = 'photos.html'
     if url == 'about.html':
         template = 'about.html'
-    return render_template(template)
+    qiniuurl = url_for_qiniu()
+    return render_template(template, url=qiniuurl)
 
 @app.route('/meweixin.html')
 def meweixin():
-    return render_template('meweixin.html')
+    qiniuurl = url_for_qiniu()
+    return render_template('meweixin.html', url=qiniuurl)
 
 @app.route('/working/')
 def work():
-    return render_template('working.html')
+    qiniuurl = url_for_qiniu()
+    return render_template('working.html', url=qiniuurl)
 
 
 @app.route('/working/<url>')
@@ -50,12 +56,14 @@ def working(url=None):
         template = 'about.html'
     if url == 'photos.html':
         template = 'photos.html'
-    return render_template(template)
+    qiniuurl = url_for_qiniu()
+    return render_template(template, url=qiniuurl)
 
 
 @app.route('/photos/')
 def photos():
-    return render_template('photos.html')
+    qiniuurl = url_for_qiniu()
+    return render_template('photos.html', url=qiniuurl)
 
 
 @app.route('/photos/<url>')
@@ -92,8 +100,12 @@ def photosURL(url=None):
         template = 'about.html'
     if url == 'meweixin.html':
         template = 'meweixin.html'
-    return render_template(template)
+    qiniuurl = url_for_qiniu()
+    return render_template(template, url=qiniuurl)
 
+
+def url_for_qiniu():
+    return 'http://7xih5z.com1.z0.glb.clouddn.com'
 
 if __name__ == '__main__':
     app.debug = True
